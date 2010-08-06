c98334f50dc4ceb13d0ac87ae41a6f59512ffb9f
Richard Smith <richard@ex-parrot.com>
2010-08-06 18:43:03 +0000

    New splices tool.  Better error when makeinfo is missing.

---
+++ apps/splices/splices.cpp
@@ -0,0 +1,123 @@
+// -*- C++ -*- splices.cpp - utility to find splices between methods
+// Copyright (C) 2010 Richard Smith <richard@ex-parrot.com>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+// $Id$
+
+#include <ringing/common.h>
+
+#include <ringing/mathutils.h>
+#include <ringing/streamutils.h>
+#include <ringing/methodset.h>
+#include <ringing/litelib.h>
+#include <ringing/group.h>
+#include <ringing/falseness.h>
+#include "args.h"
+
+#include <iostream>
+
+RINGING_USING_NAMESPACE
+RINGING_USING_STD
+
+struct arguments
+{
+  init_val<int,0> bells;
+
+  init_val<bool,false> null_splices;
+
+  void bind( arg_parser& p );
+  bool validate( arg_parser& p );
+};
+
+
+void arguments::bind( arg_parser& p )
+{
+  p.add( new help_opt );
+  p.add( new version_opt );
+           
+  p.add( new integer_opt
+         ( 'b', "bells",
+           "The number of bells.  This option is required", "BELLS",
+           bells ) );
+
+  p.add( new boolean_opt
+         ( 'n', "null-splice",
+           "Display pairs of methods with no splice", 
+           null_splices ) );
+}
+
+bool arguments::validate( arg_parser& ap )
+{               
+  if ( bells <= 0 )
+    {
+      ap.error( "Then number of bells must be positive" );
+      return false;
+    }
+
+  if ( bells >= int(bell::MAX_BELLS) )
+    {
+      ap.error( make_string() << "The number of bells must be less than "
+                << bell::MAX_BELLS );
+      return false;
+    }
+
+  return true;
+}
+
+string describe_splice( group const& sg )
+{
+  return make_string() << (sg.size()/2) << "-lead";
+}
+
+int main( int argc, char *argv[] )
+{
+  arguments args;
+
+  {
+    arg_parser ap( argv[0], "musgrep -- grep rows for music", "OPTIONS" );
+    args.bind( ap );
+
+    if ( !ap.parse(argc, argv) )
+      {
+        ap.usage();
+        return 1;
+      }
+    
+    if ( !args.validate(ap) )
+      return 1;
+  }
+
+  // Read methods into memory -- we need to make multiple passes over
+  // the list and we can't do that on standard input.
+  methodset meths;
+  {
+    meths.store_facet< litelib::payload >();
+    litelib in( args.bells, cin );
+    copy( in.begin(), in.end(), back_inserter(meths) );  
+  }
+
+  for ( methodset::const_iterator i=meths.begin(), e=meths.end(); i!=e; ++i ) {
+    methodset::const_iterator j=i;  ++j;
+    for ( ; j != e; ++j ) {
+      group sg( falseness_table( i->meth(), j->meth() ).generate_group() );
+      if ( sg.size() < factorial(args.bells-1) || args.null_splices ) {
+        string a = i->get_facet< litelib::payload >();
+        string b = j->get_facet< litelib::payload >();
+        cout << a << "\t" << b << "\t" << describe_splice(sg) << "\n";
+      }
+    }
+  }
+}
