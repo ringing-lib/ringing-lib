9a60643e293093e7982404404688b982dd451d11
Richard Smith <richard@ex-parrot.com>
2019-02-12 18:41:20 +0000

    Allow extended bells

--- ringing/place_notation.h
+++ ringing/place_notation.h
@@ -1,5 +1,6 @@
 // -*- C++ -*- place_notation.h - Functions for parsing placenotation
-// Copyright (C) 2001, 2009 Martin Bright <martin@boojum.org.uk>
+// Copyright (C) 2001, 2009, 2019 Martin Bright <martin@boojum.org.uk> and
+// Richard Smith <richard@ex-parrot.com>
 
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -65,56 +66,88 @@ struct place_notation {
     invalid();
     invalid(const string& s);
   };
-};  
+};
 
 #if RINGING_AS_DLL && defined(_MSC_VER)
 #pragma warning( pop )
 #endif
 
+RINGING_START_DETAILS_NAMESPACE
+template <class ForwardIterator>
+void skip_ws(ForwardIterator& first, ForwardIterator last) {
+  while (first != last && isspace(*first)) ++first;
+}
+
+// Utility function to skip a single character separator, optionally
+// surrounded with white space, and return true if it was found.
+template <class ForwardIterator>
+bool skip_sep(ForwardIterator& first, ForwardIterator last, char sep) {
+  bool found = false;
+  skip_ws(first, last);
+  if (first != last && *first == sep) { found = true; ++first; }
+  skip_ws(first, last);
+  return found;
+}
+RINGING_END_DETAILS_NAMESPACE
+
+template <class ForwardIterator>
+ForwardIterator read_bell_expr(ForwardIterator first, ForwardIterator last) {
+  if ( *first == '{' ) {  
+    ++first;
+    for ( int depth = 0; first < last && !(*first == '}' && depth == 0);
+          ++first ) {
+      if (*first == '{') ++depth;
+      else if (*last == '}') --depth;
+    }
+    if ( first == last || *first != '}') throw bell::invalid();
+    ++first;
+  }
+  return first;
+}
+
 // Take the place notation between start and last, and send it as
 // a sequence of changes to out.
-template<class OutputIterator, class ForwardIterator>
+template <class OutputIterator, class ForwardIterator>
 void interpret_pn(int num, ForwardIterator first, ForwardIterator last,
-                  OutputIterator out)
-{
-  while(first != last && isspace(*first)) ++first; // Skip whitespace
-  while(first != last) {
+                  OutputIterator out) {
+  RINGING_DETAILS_PREFIX skip_ws(first, last);
+  while (first != last) {
     list<change> block;
     // See whether it's a symmetrical block or not
     bool symblock = (*first == '&');
     // Allow MicroSIRIL style '+' prefix
-    if(*first == '&' || *first == '+') {
+    if (*first == '&' || *first == '+') {
       ++first;
-      while(first != last && isspace(*first)) ++first; // Skip whitespace
-      if(first != last && *first == '.') ++first; // Skip a '.' separator
-      while(first != last && isspace(*first)) ++first; // Skip whitespace
+      // Arguably we shouldn't allow a . immediately after the & or +.
+      RINGING_DETAILS_PREFIX skip_sep(first, last, '.');
     }
-    while(first != last && (isalnum(*first) || *first == '-')) {
-      // Get a change
-      if(*first == 'X' || *first == 'x' || *first == '-') {
+    while (first != last && (isalnum(*first) || *first == '-' 
+                                             || *first == '{')) {
+      if (*first == 'X' || *first == 'x' || *first == '-') {
         ++first;
         block.push_back(change(num,"X"));
       }
       else {
         ForwardIterator j(first);
-        while(j != last && isalnum(*j) && *j != 'X' && *j != 'x') ++j;
-        if(j != first) block.push_back(change(num, string(first, j)));
+        while (j != last) {
+          if (*j == '{') j = read_bell_expr(j, last);
+          else if (isalnum(*j) && *j != 'X' && *j != 'x') ++j;
+          else break;
+        }
+        if (j != first) block.push_back(change(num, string(first, j)));
         first = j;
       }
-      while(first != last && isspace(*first)) ++first; // Skip whitespace
-      if(first != last && *first == '.') ++first; // Skip a '.' separator
-      while(first != last && isspace(*first)) ++first; // Skip whitespace
+      RINGING_DETAILS_PREFIX skip_sep(first, last, '.');
     }
     // Now output the block
     copy(block.begin(), block.end(), out);
-    if(symblock && !block.empty()) {
+    if (symblock && !block.empty()) {
       block.pop_back();
       copy(block.rbegin(), block.rend(), out);
     }
-    if(first != last) {
+    if (first != last) {
       if (*first != ',') throw place_notation::invalid( string(1u, *first) );
-      ++first; // Skip a ',' separator
-      while(first != last && isspace(*first)) ++first; // Skip whitespace
+      RINGING_DETAILS_PREFIX skip_sep(first, last, ',');
     }
   }
 }
